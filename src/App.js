d444df1b43c5a8ce6bc02961acdeaabda528a7fe
Joseph Vasquez <joetv.tech@gmail.com>
2021-05-22 21:41:34 -0700

    Imported pixijs, note: some dependency vulnerabilities which cannot be fixed with npm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import React from "react";
+import { Stage, Sprite } from "@inlet/react-pixi";
 
 function App() {
+
+  () => {
+    const reducer = (_, { data }) => data
+    const Bunny = () => {
+      const [motion, update] = useReducer(reducer)
+      const iter = useRef(0)
+      useTick(delta => {
+        const i = (iter.current += 0.05 * delta)
+        update({
+          type: 'update',
+          data: {
+            x: Math.sin(i) * 100,
+            y: Math.sin(i / 1.5) * 100,
+            rotation: Math.sin(i) * Math.PI,
+            anchor: Math.sin(i / 2),
+          },
+        })
+      })
+      return (
+        <Sprite
+          image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+          {...motion}
+        />
+      )
+    }
+    return (
+      <Stage width={300} height={300} options={{ backgroundAlpha: 0 }}>
+        <Container x={150} y={150}>
+          <Bunny />
+        </Container>
+      </Stage>
+    )
+  }
+  
   return (
     <div className="App">
       <h1>Pixi Example</h1>
+      <Stage>
+        <Sprite image="./bunny.png" x={100} y={100} />
+      </Stage>
     </div>
   );
 }
